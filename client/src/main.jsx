938ed90aec0bb0632c425e04b079ec5e83b54d0e
maheswari06112004 <maheswari6official@gmail.com>
2025-05-29 15:02:24 +0530

    initialized

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx'; // This imports your App component
+import './index.css'; // Your global CSS file
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App /> {/* Your entire application starts here */}
+  </React.StrictMode>,
+);
