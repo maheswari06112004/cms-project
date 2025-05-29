8fb2456e4a24670257a9ca4efc3cdb658ffacec4
josikaM <josikamurthy4@gmail.com>
2025-05-29 14:37:12 +0530

    updated

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+// import {Route,Routes} from "react-router-dom";
 
-function AppRoutes() {
-    return (
-        <Routes>
-            
-        </Routes>
-    );
-}
+// function AppRoutes() {
+//     return (
+//         <Routes>
+//             <Route path="/" element={<BasicLayouts/>}>
+//         </Routes>
+//     )
+// }
 
-export default AppRoutes;
+// export default AppRoutes;
